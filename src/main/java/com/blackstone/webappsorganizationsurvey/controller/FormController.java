94f2bd0ade7e42cbbde60f563f43870ed4a0d0f1
Rami Mohsen <rmohsen@blackstoneeit.com>
2022-01-31 10:29:48 +0200

    fix file upload

--- src/main/java/com/blackstone/webappsorganizationsurvey/controller/FormController.java
+++ src/main/java/com/blackstone/webappsorganizationsurvey/controller/FormController.java
@@ -2,17 +2,15 @@ package com.blackstone.webappsorganizationsurvey.controller;
 
 
 import com.blackstone.webappsorganizationsurvey.dto.FormRequest;
-import com.blackstone.webappsorganizationsurvey.dto.FormResponse;
 import com.blackstone.webappsorganizationsurvey.entity.Form;
 import com.blackstone.webappsorganizationsurvey.service.IFormService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,11 +23,11 @@ public class FormController {
     @ApiOperation(value = "Submit survey form",
             notes = "Submit Survey form"
     )
-    @PostMapping("/submit")
+    @PostMapping(value = "/submit", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     @ResponseStatus(HttpStatus.CREATED)
-    public Form submitForm(@RequestParam(value = "contractFiles") MultipartFile[] contractFiles,
-                           @RequestParam(value = "systemImages", required = false) MultipartFile[] systemImages,
-                           @RequestParam(value = "securityProtocolsDocuments", required = false)
+    public Form submitForm(@RequestPart(value = "contractFiles") MultipartFile[] contractFiles,
+                           @RequestPart(value = "systemImages", required = false) MultipartFile[] systemImages,
+                           @RequestPart(value = "securityProtocolsDocuments", required = false)
                                    MultipartFile[] securityProtocolsDocuments,
                            @RequestParam("form") FormRequest formRequest) throws Exception {
 
